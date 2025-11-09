126652a63db65696ed45025aea65cf30a4478dbd
JulioPeixoto <juliorafaelnft@hotmail.com>
2025-11-08 22:25:06 -0300

    init

---
+++ src/settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    model_config = {
+        "env_file": ".env",
+        "env_file_encoding": "utf-8",
+    }
+
+    OPENAI_API_KEY: str
+
+
+settings = Settings()
